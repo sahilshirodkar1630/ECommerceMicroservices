9ba21410431b6a9415a837ffb2a0f401d8354468
Sahil Shirodkar <sahilshirodkar1630@gmail.com>
2026-07-03 19:35:02 +0530

    order service

---
+++ order-service/src/main/java/com/springboot/ecommerce/order_service/entity/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.springboot.ecommerce.order_service.entity;
+
+public enum OrderStatus {
+    CONFIRMED,CANCELLED,PENDING,DELIVERED
+}
---
+++ order-service/src/main/resources/data.sql
@@ -0,0 +1,27 @@
+INSERT INTO orders (total_price,order_status) VALUES
+(100.50,'PENDING'),
+(200.50,'CONFIRMED'),
+(300.50,'DELIVERED'),
+(150.25,'PENDING'),
+(120.50,'CANCELLED'),
+(210.50,'PENDING'),
+(300.50,'DELIVERED'),
+(110.50,'CONFIRMED'),
+(180.20,'PENDING'),
+(250.50,'CANCELLED');
+
+INSERT INTO order_item (order_id,product_id,quantity) VALUES
+(1,101,2),
+(1,102,1),
+                                                          (2,103,1),
+                                                          (2,104,3),
+                                                          (3,105,1),
+                                                          (1,106,2),
+                                                          (4,107,2),
+                                                          (5,108,3),
+                                                          (6,109,2),
+                                                          (7,110,1),
+                                                          (8,111,2),
+                                                          (9,112,3),
+                                                          (10,113,2);
+
